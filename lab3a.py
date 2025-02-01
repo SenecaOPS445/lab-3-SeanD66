ddaa12067ab083674a01b96d1742d7cdb831cb96
Sean Daweng <sdaweng@myseneca.ca>
2025-01-31 20:35:46 -0500

    Uploading all files

---
+++ lab3a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#return_text_value() function
+#Author ID: 100851187
+
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name 
+    return greeting
+text = return_text_value()
+print(text)
+
+#return_number_value() function
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+number = return_number_value()
+print('my number is ' , number)
+print('my number is ' + str(number))
+print('my number is ' + str(return_number_value()))
+
+#Main Program
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
